addc1d85c577676011c5b1858b74266eb663ee06
Abdul Raouf <raoufiscoder1033@gmail.com>
2023-11-01 19:51:42 +0500

    up

---
+++ day2/day2.js
@@ -0,0 +1,20 @@
+
+
+function registerFunction(){
+    console.log("RegisterFunction called")
+}
+
+
+// Function with Parameters
+function sum(a, b){
+    console.log(a+b);
+}
+
+function product(a, b){
+    console.log(a*b);
+}
+
+// Square
+function square(num){
+    console.log(num * num);
+}
